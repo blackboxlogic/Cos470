718d4a818b71621313d08d4f00d6dbd36e319f84
blackboxlogic <blackboxlogic@gmail.com>
2019-09-12 09:53:29 -0400

    Adding demo for Linq and FluentInts

---
+++ Notes/2019-09-10 FluentInts.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+	public static class FluentInts
+	{
+		public static int DoStuff()
+		{
+			int x = 100;
+			x = Increment(x);
+			x = Double(x);
+			x = Increment(x);
+			x = Tripple(x);
+			x = Half(x);
+			x = AddRandom(x, 10);
+			x = Quadruple(x);
+			x = AddRandom(x, 10);
+			x = Half(x);
+			x = Decrement(x);
+
+			x.Increment().Double().Increment().Tripple().Half().AddRandom(10);
+
+			return x;
+		}
+
+		public static int Double(this int x)
+		{
+			return x * 2;
+		}
+
+		public static int Tripple(this int x)
+		{
+			return x * 3;
+		}
+
+		public static int Quadruple(this int x)
+		{
+			return x * 4;
+		}
+
+		public static int AddRandom(this int x, int max)
+		{
+			Random r = new Random();
+			return x + r.Next(max);
+		}
+
+		public static int Increment(this int x)
+		{
+			return x + 1;
+		}
+
+		public static int Decrement(this int x)
+		{
+			return x + 1;
+		}
+
+		public static int Half(this int x)
+		{
+			return x / 2;
+		}
+	}
+}
---
+++ Notes/2019-09-10 LinqDemo.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+	public static class LinqDemo
+	{
+		//static string AverageNameLength(string[] names)
+		//static string LongestName(string[] names)
+		//static string[] NamesAlphabeticalByTheirReverse(string[] names)
+		//static string[] NamesThatArePalindromes(string[] names)
+		//static char MostCommonLetter(string[] names)
+		//static int AverageNumberOfVowels(string[] names)
+		//static Dictionary<char, int> CharacterFrequency(string[] names)
+		//static char LetterThanOccursMostInASingleName(string[] names)
+		public static string[] Names = new[] { "Alex", "Josh", "Daniel", "Miguel", "Nick", "Jesse", "Shea", "Lincoln", "Jon", "Manda", "Dennis", "Zachary", "Bobby", "Luke", "Fazil", "Jennifer", "Dejan" };
+
+		public static void DoStuff(IEnumerable<int> data)
+		{
+			// No data is not data, it is a process for getting data
+			IEnumerable<cat> notData = data.Distinct()
+				.TakeLast(5)
+				.OrderBy(x => x)
+				.Where(x => x % 3 == 0)
+				.Select(a => new cat(a));
+
+			cat[] cats = notData.ToArray(); // enumerating the collection creates cats and stores them.
+			var count = cats.Count(); // I can count the count the cats.
+			var reCount = notData.Count(); // If I enumerate the collection again, I make new cats.
+		}
+	}
+
+	public class cat
+	{
+		private string Name;
+
+		public cat(int name)
+		{
+			Name = name.ToString();
+			Console.Beep();
+		}
+	}
+}
