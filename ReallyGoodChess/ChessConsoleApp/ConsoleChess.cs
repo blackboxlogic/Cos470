c32c1af6a53ae08a43ad5ce9f13834b83e74c5ac
Jennifer Stone <jennifervstone@gmail.com>
2019-11-21 12:10:17 -0500

    Added Bishop, King, and Rook to the board

--- ReallyGoodChess/ChessConsoleApp/ConsoleChess.cs
+++ ReallyGoodChess/ChessConsoleApp/ConsoleChess.cs
@@ -19,6 +19,22 @@ namespace ChessConsoleApp
             Game.Board[0][0, 6] = new Knight() { Color = Color.White, Location = new Vector(0, 6) };
             Game.Board[0][7, 1] = new Knight() { Color = Color.Black, Location = new Vector(7, 1) };
             Game.Board[0][7, 6] = new Knight() { Color = Color.Black, Location = new Vector(7, 6) };
+
+            Game.Board[0][0, 2] = new Bishop() {Color = Color.White, Location = new Vector(0, 2) };
+            Game.Board[0][0, 5] = new Bishop() { Color = Color.White, Location = new Vector(0, 5) };
+            Game.Board[0][7, 2] = new Bishop() { Color = Color.Black, Location = new Vector(7, 2) };
+            Game.Board[0][7, 5] = new Bishop() { Color = Color.Black, Location = new Vector(7, 5) };
+
+            Game.Board[0][0, 0] = new Rook() { Color = Color.White, Location = new Vector(0, 0) };
+            Game.Board[0][0, 7] = new Rook() { Color = Color.White, Location = new Vector(0, 7) };
+            Game.Board[0][7, 0] = new Rook() { Color = Color.Black, Location = new Vector(7, 0) };
+            Game.Board[0][7, 7] = new Rook() { Color = Color.Black, Location = new Vector(7, 7) };
+
+
+            Game.Board[0][0, 4] = new King() { Color = Color.White, Location = new Vector(0, 4) };
+            Game.Board[0][7, 4] = new King() { Color = Color.Black, Location = new Vector(7, 4) };
+
+
             for (int i = 0; i < 8; i++)
             {
                 Game.Board[0][1, i] = new Pawn() { Color = Color.White, Location = new Vector(1, i) };
